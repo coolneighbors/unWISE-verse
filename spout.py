dfc0f728378bf7559f98903b0dd47be1dd80e645
Noah Schapera <noahschapera.9@gmail.com>
2022-06-07 11:50:30 -0700

    Changed order of spout to make more sense

--- spout.py
+++ spout.py
@@ -9,8 +9,7 @@ import os
 import csv
 import wv
 
-projectID = input("Input Project ID: ")
-setName = input("Input Subject Set Name: ")
+
 
 os.system('panoptes configure')
 
@@ -19,6 +18,8 @@ pub=''
 
 header = ['RA', 'DEC', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10']
 f = open('manifest.csv', 'w', newline='')
+
+
 writer= csv.writer(f)
 
 
@@ -35,9 +36,8 @@ with open('targets.csv', newline='') as targetList:
         #set WV parameters to RA and DEC
         wv.custom_params(RA, DEC)
         
-        
         #Save all images for parameter set
-        flist = wv.png_set(RA, DEC, "pngs")
+        flist = wv.png_set(RA, DEC, "pngs", scale_factor=2)
         
         #os.system(f"python one_wiseview_gif.py --outdir pngs --keep_pngs {RA} {DEC} {gifName}")
         
@@ -48,20 +48,32 @@ with open('targets.csv', newline='') as targetList:
         print(f"Added Manifest Line for Target {RA}, {DEC}")
 
 f.close()
+
+
 print('Generation Complete')
 
 pub=input("Publish to zooniverse? y/n : ")
 
 if pub == 'y':
+    
+    projectID = input("Input Project ID: ")
+    setName = input("Input Subject Set Name: ")
+
+    os.system('panoptes configure')
+    
     newset=input('Create new subject set? y/n : ')
+    
     if newset == 'y':
+        
         os.system(f"panoptes subject-set create {projectID} '{setName}'")
+        
         setNum=input('Input subject set number : ')
         os.system(f"panoptes subject-set upload-subjects {setNum} manifest.csv")
         
     else:
         setNum=input('Input subject set number : ')
         os.system(f"panoptes subject-set upload-subjects {setNum} manifest.csv")
+        
 else:
     print('Program Complete')
     
