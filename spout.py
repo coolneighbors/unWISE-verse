c87030d357cf645e44b2b137b96eb8f3a157627b
Noah Schapera <noahschapera.9@gmail.com>
2022-06-08 11:22:32 -0700

    Moved publication hander to new script, verified inputs. #iss4 #iss2

--- spout.py
+++ spout.py
@@ -5,23 +5,14 @@ Created on Fri Jun  3 13:25:59 2022
 @author: Noah Schapera
 """
 
-import os
 import csv
 import wv
-
-
-
-os.system('panoptes configure')
-
-
-pub=''
+import pubScript
 
 header = ['RA', 'DEC', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10']
 f = open('manifest.csv', 'w', newline='')
 
-
 writer= csv.writer(f)
-
 
 writer.writerow(header)
 print('Header Created')
@@ -37,7 +28,7 @@ with open('targets.csv', newline='') as targetList:
         wv.custom_params(RA, DEC)
         
         #Save all images for parameter set
-        flist = wv.png_set(RA, DEC, "pngs", scale_factor=2)
+        flist = wv.png_set(RA, DEC, "pngs", scale_factor=3)
         
         #os.system(f"python one_wiseview_gif.py --outdir pngs --keep_pngs {RA} {DEC} {gifName}")
         
@@ -51,29 +42,6 @@ f.close()
 
 
 print('Generation Complete')
-
-pub=input("Publish to zooniverse? y/n : ")
-
-if pub == 'y':
-    
-    projectID = input("Input Project ID: ")
-    setName = input("Input Subject Set Name: ")
-
-    os.system('panoptes configure')
-    
-    newset=input('Create new subject set? y/n : ')
-    
-    if newset == 'y':
-        
-        os.system(f"panoptes subject-set create {projectID} '{setName}'")
-        
-        setNum=input('Input subject set number : ')
-        os.system(f"panoptes subject-set upload-subjects {setNum} manifest.csv")
-        
-    else:
-        setNum=input('Input subject set number : ')
-        os.system(f"panoptes subject-set upload-subjects {setNum} manifest.csv")
-        
-else:
-    print('Program Complete')
+pubScript.pubHandler()
+print('Program Complete')
     
