23962923cbaa228f84f7e26db96dd74b85fb2b88
austinh2001 <austinh2001@gmail.com>
2023-06-05 15:41:48 -0400

    Attempting another solution

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import pip
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -15,3 +16,8 @@ setup(
     description='An integrated unWISE data collection and Zooniverse upload pipeline using the Panoptes-Client.',
     long_description=open('README.md').read(),
 )
+
+# This is to avoid the following error: https://github.com/zooniverse/panoptes-python-client/issues/264
+pip.main(['install', 'python-magic==0.4.27'])
+pip.main(['uninstall', 'python-magic-bin==0.4.14'])
+pip.main(['install', 'python-magic-bin==0.4.14'])
