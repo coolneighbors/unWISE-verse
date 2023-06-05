80236a158f8b3fd2b2189176f98070b3e0020d40
austinh2001 <austinh2001@gmail.com>
2023-06-05 15:28:49 -0400

    Avoiding warning
    If the python-magic-bin library is installed after the python-magic library, an error will continuously display in the console: "WARNING:panoptes_client:Broken libmagic installation detected..."
    
    This should hopefully allow for python-magic to always be installed first.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Aaron Meisner',
     author_email='aaron.meisner@noirlab.edu',
     packages=['unWISE-verse'],
+    setup_requires=['python-magic==0.4.27'],
     install_requires=requirements,
     version='1.0',
     license='MIT',
