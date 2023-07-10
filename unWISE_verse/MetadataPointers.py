395342a0d692a1c4e1eaa05de52e2155d2a5c199
austinh2001 <austinh2001@gmail.com>
2023-07-10 17:47:03 -0400

    Fixed typo in MetadataPointers

--- unWISE_verse/MetadataPointers.py
+++ unWISE_verse/MetadataPointers.py
@@ -8,8 +8,8 @@ def generate_SIMBAD_url(RA, DEC, radius = 60):
     return SIMBAD_template_url.format(RA,DEC,radius)
 
 def generate_VizieR_url(RA, DEC,FOV):
-    SIMBAD_template_url = "https://vizier.u-strasbg.fr/viz-bin/VizieR?-c={}+{}&-c.bs={}x{}&-out.add=_r&-out.add=_RAJ%2C_DEJ&-sort=_r&-to=&-out.max=20&-meta.ucd=2&-meta.foot=1&-oc.form=d"
-    return SIMBAD_template_url.format(RA, DEC, FOV, FOV)
+    VizieR_template_url = "https://vizier.u-strasbg.fr/viz-bin/VizieR?-c={}+{}&-c.bs={}x{}&-out.add=_r&-out.add=_RAJ%2C_DEJ&-sort=_r&-to=&-out.max=20&-meta.ucd=2&-meta.foot=1&-oc.form=d"
+    return VizieR_template_url.format(RA, DEC, FOV, FOV)
 
 def generate_IRSA_url(RA, DEC):
     IRSA_template_url = "https://irsa.ipac.caltech.edu/applications/finderchart/servlet/api?mode=getResult&subsetsize=11.7333&locstr={}+{}&survey=DSS%2CSDSS%2C2MASS%2CSEIP%2CWISE"
