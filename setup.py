e7b4d15bc9add4ccd5c7afe73a06665ded223777
austinh2001 <austinh2001@gmail.com>
2023-07-17 15:14:43 -0400

    Added fix to python-magic import bug

--- setup.py
+++ setup.py
@@ -1,8 +1,44 @@
+import subprocess
 from setuptools import setup
 
 with open('requirements.txt', encoding="utf-16") as f:
     requirements = f.read().splitlines()
-    print(requirements)
+
+# This fixes an import bug with the python-magic and python-magic-bin packages.
+# https://github.com/zooniverse/panoptes-python-client/issues/264
+def fix_python_magic_install_order():
+    python_magic = [line for line in requirements if 'python-magic' in line]
+    if(len(python_magic) != 1):
+        for p in python_magic:
+            if("bin" in p):
+                python_magic.remove(p)
+        if(len(python_magic) != 1):
+            raise Exception(f'python-magic requirement not found in requirements.txt or found multiple times: \'{python_magic}\'')
+    python_magic = python_magic[0]
+    python_magic_version = python_magic.split('==')[1]
+
+    if(len(python_magic_version) != 0):
+        subprocess.call(['pip', 'install', f'python-magic=={python_magic_version}'])
+    else:
+        subprocess.call(['pip', 'install', 'python-magic'])
+
+    python_magic_bin = [line for line in requirements if 'python-magic-bin' in line]
+    if(len(python_magic_bin) != 1):
+        raise Exception(f'python-magic-bin requirement not found in requirements.txt or found multiple times: \'{python_magic_bin}\'')
+
+    python_magic_bin = python_magic_bin[0]
+    python_magic_bin_version = python_magic_bin.split('==')[1]
+    if(len(python_magic_bin_version) != 0):
+        subprocess.call(['pip', 'uninstall', f'python-magic-bin=={python_magic_bin_version}'])
+        subprocess.call(['pip', 'install', f'python-magic-bin=={python_magic_bin_version}'])
+    else:
+        subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
+        subprocess.call(['pip', 'install', 'python-magic-bin'])
+
+    requirements.remove(python_magic)
+    requirements.remove(python_magic_bin)
+
+fix_python_magic_install_order()
 
 setup(
     name='unWISE_verse',
