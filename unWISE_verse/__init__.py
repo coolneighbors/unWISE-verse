4ebc8a56b9ab64262ce5b9713b9e728f2acb5a48
austinh2001 <austinh2001@gmail.com>
2023-07-25 13:03:28 -0400

    Update __init__.py

--- unWISE_verse/__init__.py
+++ unWISE_verse/__init__.py
@@ -9,5 +9,6 @@ A pipeline for downloading, modifying, and uploading unWISE images to Zooniverse
 import subprocess
 # This fixes an import bug with the python-magic and python-magic-bin packages.
 # https://github.com/zooniverse/panoptes-python-client/issues/264
+subprocess.call(['pip', 'install', 'python-magic'])
 subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
 subprocess.call(['pip', 'install', 'python-magic-bin'])
