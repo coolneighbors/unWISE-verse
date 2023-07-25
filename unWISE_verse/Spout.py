f43ce377b0d93385974e14b1af709215d0453ab5
austinh2001 <austinh2001@gmail.com>
2023-07-25 12:40:11 -0400

    Moved libmagic bug solution to __init__.py

--- unWISE_verse/Spout.py
+++ unWISE_verse/Spout.py
@@ -20,11 +20,6 @@ from panoptes_client import Panoptes, Project, SubjectSet, Subject, User
 from panoptes_client.panoptes import PanoptesAPIException
 from panoptes_client.set_member_subject import SetMemberSubject
 
-# This fixes an import bug with the python-magic and python-magic-bin packages.
-# https://github.com/zooniverse/panoptes-python-client/issues/264
-subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
-subprocess.call(['pip', 'install', 'python-magic-bin'])
-
 import unWISE_verse
 
 from unWISE_verse import Manifest
