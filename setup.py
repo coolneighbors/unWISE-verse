d14e9bc815c8c71e07e8522f599ac3f52a3a3baa
austinh2001 <austinh2001@gmail.com>
2023-07-25 13:39:06 -0400

    Added libmagic solution to end of setup. Should hopefully be the last commit regarding this issue.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import subprocess
 from setuptools import setup
 
 with open('requirements.txt', encoding="utf-16") as f:
@@ -15,3 +16,9 @@ setup(
     description='An integrated unWISE data collection and Zooniverse upload pipeline using the Panoptes-Client.',
     long_description=open('README.md').read(),
 )
+
+# This fixes an import bug with the python-magic and python-magic-bin packages.
+# https://github.com/zooniverse/panoptes-python-client/issues/264
+subprocess.call(['pip', 'install', 'python-magic'])
+subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
+subprocess.call(['pip', 'install', 'python-magic-bin'])
--- unWISE_verse/__init__.py
+++ unWISE_verse/__init__.py
@@ -5,10 +5,3 @@ unWISE_verse
 A pipeline for downloading, modifying, and uploading unWISE images to Zooniverse.
 
 """
-
-import subprocess
-# This fixes an import bug with the python-magic and python-magic-bin packages.
-# https://github.com/zooniverse/panoptes-python-client/issues/264
-subprocess.call(['pip', 'install', 'python-magic'])
-subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
-subprocess.call(['pip', 'install', 'python-magic-bin'])
