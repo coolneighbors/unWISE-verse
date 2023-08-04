cdfcce3bfeadbfab94cef99c3bcb2ed22270678c
austinh2001 <austinh2001@gmail.com>
2023-08-04 15:17:43 -0400

    Trying to fix import of themes folder

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     author_email='aaron.meisner@noirlab.edu',
     packages=['unWISE_verse'],
     install_requires=requirements,
+    data_files=[('unWISE_verse', ['unWISE_verse/themes'])],
     version='1.2',
     license='MIT',
     description='An integrated unWISE data collection and Zooniverse upload pipeline using the Panoptes-Client.',
