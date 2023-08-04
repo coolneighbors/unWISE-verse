93a21d88078bb741f06707c532e3c2f7434d6c78
austinh2001 <austinh2001@gmail.com>
2023-08-04 14:53:06 -0400

    OS type fix

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setup(
 
 # This fixes an import bug with the python-magic and python-magic-bin packages.
 # https://github.com/zooniverse/panoptes-python-client/issues/264
-subprocess.call(['pip', 'install', 'python-magic'])
-subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
-subprocess.call(['pip', 'install', 'python-magic-bin'])
+import platform
+
+if platform.system() == 'Windows':
+    subprocess.call(['pip', 'install', 'python-magic'])
+    subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
+    subprocess.call(['pip', 'install', 'python-magic-bin'])
